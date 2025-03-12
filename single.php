e1d6d0e890e022ef200347db15908845c124b73b
ChillibyteUK <dan@chillibyte.co.uk>
2025-03-12 15:01:36 +0000

    single insights

--- single.php
+++ single.php
@@ -2,70 +2,71 @@
 // Exit if accessed directly.
 defined('ABSPATH') || exit;
 get_header();
-$img = get_the_post_thumbnail_url(get_the_ID(), 'full');
+$img = get_the_post_thumbnail(get_the_ID(), 'full');
 ?>
 <main id="main" class="blog">
+    <div class="page_hero">
+        <?=$img?>
+    </div>
+    <section class="translucent_text--light">
+        <div class="container p-5">
+            <div class="has-red-400-color">Insights</div>
+            <?php
+            if (function_exists('yoast_breadcrumb')) {
+                yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
+            }
+?>
+        </div>
+    </section>
     <?php
     $content = get_the_content();
-    $blocks = parse_blocks($content);
-    $sidebar = array();
-    $after;
+$blocks = parse_blocks($content);
+$sidebar = array();
+$after;
 ?>
-    <section class="breadcrumbs container-xl">
-        <?php
-if (function_exists('yoast_breadcrumb')) {
-    yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
-}
-?>
-    </section>
-    <div class="container-xl">
-        <div class="row g-4 pb-4">
-            <div class="col-lg-9">
-                <h1 class="blog__title"><?=get_the_title()?></h1>
-                <img src="<?=$img?>" alt="" class="blog__image">
+    <div class="container p-5 bg--white">
+        <div class="row">
+            <div class="col-md-8">
+            <h1 class="h2"><?=get_the_title()?></h1>
                 <?php
         $count = estimate_reading_time_in_minutes(get_the_content(), 200, true, true) ?? null;
-        if ($count) {
-            echo $count;
-        }
+if ($count) {
+    echo $count;
+}
 
 foreach ($blocks as $block) {
     echo render_block($block);
 }
 ?>
             </div>
-            <div class="col-lg-3">
-                <div class="sidebar">
-
-                    <div class="h5 d-none d-lg-block">Related News</div>
-                    <div class="related">
-                        <?php
-        $cats = get_the_category();
-$ids = wp_list_pluck($cats, 'term_id');
-$r = new WP_Query(array(
-    'category__in' => $ids,
-    'posts_per_page' => 4,
-    'post__not_in' => array(get_the_ID())
-));
+            <div class="col-md-4">
+                <?=$img?>
+            </div>
+        </div>
+    </div>
+    <div class="container latest_insights has-grey-100-background-color p-5">
+        <div class="h2 mb-5">Further reading</div>
+        <div class="row w-100">
+            <?php
+            $r = new WP_Query(array(
+                'posts_per_page' => 4,
+                'post__not_in' => array(get_the_ID())
+            ));
 while ($r->have_posts()) {
     $r->the_post();
     ?>
-                        <a class="related__card"
-                            href="<?=get_the_permalink()?>">
-                            <img src="<?=get_the_post_thumbnail_url(get_the_ID(), 'large')?>"
-                                alt="" class="related__image">
-                            <div class="related__content">
-                                <h3 class="related__title">
-                                    <?=get_the_title()?>
-                                </h3>
-                            </div>
-                        </a>
-                        <?php
+            <div class="col-md-4">
+            <a class="latest_insights__card" href="<?=get_the_permalink()?>">
+                        <?=get_the_post_thumbnail($q->ID, 'large', ['class' => 'latest_insights__image'])?>
+                        <h3><?=get_the_title()?></h3>
+                        <div class="latest_insights__intro">
+                            <?=get_field('post_excerpt', get_the_ID()) ?: wp_trim_words(get_the_content(), 30)?>
+                        </div>
+                    </a>
+            </div>
+                <?php
 }
 ?>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 </main>
