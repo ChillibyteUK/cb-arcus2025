d91158a564568b274d47a72b313f26916d02383d
ChillibyteUK <dan@chillibyte.co.uk>
2025-03-14 10:26:12 +0000

    button class

--- page-templates/blocks/cb_button.php
+++ page-templates/blocks/cb_button.php
@@ -3,13 +3,15 @@ $l = get_field('link') ?? null;
 
 $f = get_field('file') ?? null;
 
+$class = $block['className'] ?? null;
+
 if ($f) {
     $title = get_field('file_button_title') ?: 'Download';
     ?>
-<a href="<?=$f['url']?>" target="_blank" class="button"><?=$title?></a>
+<a href="<?=$f['url']?>" target="_blank" class="button <?=$class?>"><?=$title?></a>
     <?php
 } else {
     ?>
-<a href="<?=$l['url']?>" target="<?=$l['target']?>" class="button"><?=$l['title']?></a>
+<a href="<?=$l['url']?>" target="<?=$l['target']?>" class="button <?=$class?>"><?=$l['title']?></a>
 <?php
 }
