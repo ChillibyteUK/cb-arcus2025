2397ebecadaa3f6e83f0232c57c26f5e9966d8e4
ChillibyteUK <dan@chillibyte.co.uk>
2025-03-14 11:37:08 +0000

    video hero

--- page-templates/blocks/cb_video_hero.php
+++ page-templates/blocks/cb_video_hero.php
@@ -1,3 +1,12 @@
+<?php
+$mp4 = get_field('video_mp4') ?? null;
+$webm = get_field('video_webm') ?? null;
+?>
 <div class="video_hero">
-    <?=wp_get_attachment_image(get_field('image'),'full')?>
+    <video autoplay loop muted playsinline>
+        <source src="<?=$webm?>" type="video/webm">
+        <source src="<?=$mp4?>" type="video/mp4">
+        <?=wp_get_attachment_image(get_field('image'), 'full', false, ['alt' => 'Video not supported'])?>
+        Your browser does not support the video tag.
+    </video>
 </div>
